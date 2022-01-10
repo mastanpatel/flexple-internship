7dde8c33ed14838235ff801a1633f3e3f0c3c2c7
Mastan Patel <m.mastanpatel@gmail.com>
2022-01-10 16:07:28 +0530

    signup module implementaed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
     def new
     
     end
+
+    def create
+        @user = User.new(user_params)
+        @user.profile = Profile.new
+        if @user.save
+          # Handle a successful save.
+          
+          flash.now[:success] = 'User Created Successfully'
+          log_in(@user)
+          redirect_to(root_path)
+        else
+          render 'new'
+        end
+      end
+
+    private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+      #
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
   get    'signup'  => 'users#new'
+  post   'signup'  => 'users#create'
   resources :users
   resources :profiles , only: [:update]
   resources :educations , only: [:new]
